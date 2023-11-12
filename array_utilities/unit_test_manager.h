49f13f230763f2becbb7703fcc8d33e164294e45
riddellriddell <riddellriddell@gmail.com>
2023-11-12 20:14:28 +1100

    added fixes to wide node linked list added unit tests for wide node linked list

--- array_utilities/unit_test_manager.h
+++ array_utilities/unit_test_manager.h
@@ -4,10 +4,14 @@
 #include "free_list.h"
 #include "wide_node_linked_list.h"
 
+#include <iostream>
 #include <limits>
 #include <cstdlib>
 #include <ctime>
 #include <assert.h>
+#include <ranges>
+#include <algorithm>
+#include <numeric>
 
 namespace ArrayUtilities
 {
@@ -43,10 +47,141 @@ namespace ArrayUtilities
 			//create wide node linked list
 			auto widenode_list = wide_node_linked_list<std::numeric_limits<uint8>::max(), uint16, 255 * 255, 8, uint8>();
 
-			widenode_list.add(0, static_cast<uint8>(69));
+			uint32 item_to_add_01 = 69; //nice
+			uint32 index_to_add_01 = 0;
 
-			//widenode_list.remove(0, static_cast<uint8>(69));
+			widenode_list.add(index_to_add_01, static_cast<uint8>(item_to_add_01));
 
+			{
+				bool found_added_item = false;
+
+				auto begin = widenode_list.get_root_node_start(index_to_add_01);
+				auto end = widenode_list.end();
+
+				std::for_each(begin, end, [&](auto& x) {found_added_item = (x == item_to_add_01); });
+
+				assert(found_added_item, "the value of x was not correctly retrieved");
+			}
+
+			//remove the value inserted 
+			widenode_list.remove(index_to_add_01, static_cast<uint8>(item_to_add_01));
+
+			{
+				bool found_added_item = false;
+
+				auto begin = widenode_list.get_root_node_start(index_to_add_01);
+				auto end = widenode_list.end();
+
+				std::for_each(begin, end, [&](auto& x) {found_added_item = (x == item_to_add_01); });
+
+				assert(!found_added_item, "the value of x should have been removed");
+			}
+
+
+			uint32 item_to_add_02 = 96; //spicy
+			uint32 index_to_add_02 = 1;
+
+			widenode_list.add(index_to_add_02, static_cast<uint8>(item_to_add_02));
+
+			{
+				bool found_added_item = false;
+
+				auto begin = widenode_list.get_root_node_start(index_to_add_02);
+				auto end = widenode_list.end();
+
+				std::for_each(begin, end, [&](auto& x) {found_added_item = (x == item_to_add_02); });
+
+				assert(found_added_item, "the value of x was not correctly retrieved");
+			}
+
+			//remove the value inserted 
+			widenode_list.remove(index_to_add_02, static_cast<uint8>(item_to_add_02));
+
+			{
+				bool found_added_item = false;
+
+				auto begin = widenode_list.get_root_node_start(index_to_add_02);
+				auto end = widenode_list.end();
+
+				std::for_each(begin, end, [&](auto& x) {found_added_item = (x == item_to_add_02); });
+
+				assert(!found_added_item, "the value of x should have been removed");
+			}
+
+			//add 9 items forcing the creation of 2 nodex 
+			constexpr uint32 items_to_add_count_03 = 9;
+			std::array<uint32, items_to_add_count_03> items_to_add_03;
+
+			uint32 index_to_add_03 = 3;
+
+			//crate array of all the items to add
+			std::iota(items_to_add_03.begin(), items_to_add_03.end(), 0);
+
+			//add all the items to the array 
+			std::for_each(items_to_add_03.begin(), items_to_add_03.end(), [&](auto& x) {widenode_list.add(index_to_add_03,x); });
+
+			//check to see if all the items were added 
+			std::for_each(items_to_add_03.begin(), items_to_add_03.end(), [&](auto& x)
+				{
+					bool found_added_item = false;
+
+					auto begin = widenode_list.get_root_node_start(index_to_add_03);
+					auto end = widenode_list.end();
+
+					std::for_each(begin, end, [&](auto& item) {found_added_item |= (item == x); });
+
+					assert(found_added_item, "the value of x should have been found");
+				});
+
+			uint32 item_to_remove_03 = 4;
+
+			//remove a single item in the middle 
+			widenode_list.remove(index_to_add_03, item_to_remove_03);
+
+			//check to see 4 is removed and all the other items remain 
+			std::for_each(items_to_add_03.begin(), items_to_add_03.end(), [&](auto& x)
+				{
+					bool found_added_item = false;
+
+					auto begin = widenode_list.get_root_node_start(index_to_add_03);
+					auto end = widenode_list.end();
+
+					std::for_each(begin, end, [&](auto& item) {found_added_item |= (item == x); });
+
+					if (x == item_to_remove_03)
+					{
+						assert(!found_added_item, "the value of item_to_remove_03 should have been removed");
+					}
+					else
+					{
+						assert(found_added_item, "the value of x should have been found");
+					}
+				});
+
+			//remove all the remaining items 
+			std::for_each(items_to_add_03.begin(), items_to_add_03.end(), [&](auto& x)
+				{
+					auto begin = widenode_list.get_root_node_start(index_to_add_03);
+					auto end = widenode_list.end();
+
+					if (x != item_to_remove_03)
+					{
+						widenode_list.remove(index_to_add_03, x);
+					}
+				});
+
+			//check all the items have been removed 
+			std::for_each(items_to_add_03.begin(), items_to_add_03.end(), [&](auto& x)
+				{
+					bool found_added_item = false;
+
+					auto begin = widenode_list.get_root_node_start(index_to_add_03);
+					auto end = widenode_list.end();
+
+					std::for_each(begin, end, [&](auto& item) {found_added_item |= (item == x); });
+
+					assert(!found_added_item, "the value of x should have been removed");
+				});
 		}
 	};
 }
--- array_utilities/wide_node_linked_list.h
+++ array_utilities/wide_node_linked_list.h
@@ -72,8 +72,8 @@ namespace ArrayUtilities
 	template<typename TLinkType, uint32 Imax_children>
 	struct root_node
 	{
-		TLinkType last_node; //the last node that is not 100 percent filled 
-		MiscUtilities::uint_s<Imax_children>::int_type_t count; //how many items are attached to this root node
+		TLinkType write_node; //the last node that is not 100 percent filled 
+		MiscUtilities::uint_s<Imax_children>::int_type_t write_index; //how many items are attached to this root node
 	};
 
 	//the idea of this class is that most entries often are inside a size 
@@ -95,6 +95,9 @@ namespace ArrayUtilities
 		//all the nodes 
 		std::array<wide_node<node_width, TLinkType, TDataType>, node_count > nodes;
 	
+		//construcotr
+		constexpr wide_node_linked_list();
+
 		//reset the state to of the linked list
 		void reset();
 
@@ -113,7 +116,7 @@ namespace ArrayUtilities
 			
 			static constexpr TLinkType invalid_node_index = std::numeric_limits<TLinkType>::max();
 			
-			uint32 count_in_node;
+			uint32 read_index;
 			uint32 node_index;
 			
 			//pointer to wide node array 
@@ -122,19 +125,19 @@ namespace ArrayUtilities
 			// Define the iterator dereference operator.
 			TDataType& operator*() const
 			{
-				return nodes[node_index]->active_node_data.data[count_in_node];
+				return (*nodes)[node_index].active_node_data.data[read_index];
 			}
 			
 			void next()
 			{
 				//check if we are at the last in the node
-				bool last_in_node = !count_in_node;
+				bool last_in_node = !read_index;
 			
 				//if we are the last in the node change the ndde we are pointing to 
-				node_index = node_index + (nodes[node_index]->active_node_data.child_node * last_in_node);
+				node_index = (node_index * !last_in_node) + ((*nodes)[node_index].active_node_data.child_node * last_in_node);
 			
-				//go to next index in node
-				count_in_node = (count_in_node -1) + (node_width * last_in_node);
+				//go to next index in node or if it is the last node reset read index to node width -1 
+				read_index = (read_index + (node_width * last_in_node)) -1;
 			}
 			
 			// Define the iterator pre-increment operator.
@@ -145,25 +148,29 @@ namespace ArrayUtilities
 			
 			// Define the iterator equality operators.
 			bool operator==(const iterator & other) const {
-				return (count_in_node == other.count_in_node) && (node_index == other.node_index);
+				return (read_index == other.read_index) && (node_index == other.node_index);
 			}
 			
 			bool operator!=(const iterator & other) const {
-				return (count_in_node != other.count_in_node) || (node_index != other.node_index);
+				return (read_index != other.read_index) || (node_index != other.node_index);
 			}
 			
 			iterator(parent_list_type& parent_list, TLinkType root_node_index) :
-				count_in_node(parent_list.root_node_ptrs[root_node_index].count % node_width), 
-				node_index(parent_list.root_node_ptrs[root_node_index].last_node), 
-				nodes(&root_node_index.nodes)
+				read_index((parent_list.root_node_ptrs[root_node_index].write_index)),
+				node_index(parent_list.root_node_ptrs[root_node_index].write_node),
+				nodes(&(parent_list.nodes))
 			{
-				next();
 			}
 			
 			//end index itterator
-			iterator() :count_in_node(node_count -1), node_index(invalid_node_index),nodes(nullptr) {}
+			iterator() :read_index(node_width -1), node_index(invalid_node_index),nodes(nullptr) {}
 		};
 	
+		//get an iterator to iterate through all the data items in a root node 
+		iterator get_root_node_start(TLinkType root_node_index);
+
+		//end itterator 
+		iterator end();
 	private:
 
 		//reset all the pointers to root nodes 
@@ -181,7 +188,11 @@ namespace ArrayUtilities
 	};
 
 
-
+	template<size_t root_node_count, typename TLinkType, size_t node_count, size_t node_width, typename TDataType>
+	inline constexpr wide_node_linked_list<root_node_count, TLinkType, node_count, node_width, TDataType>::wide_node_linked_list()
+	{
+		reset();
+	}
 
 	template<size_t root_node_count, typename TLinkType, size_t node_count, size_t node_width, typename TDataType>
 	inline void wide_node_linked_list<root_node_count, TLinkType, node_count, node_width, TDataType>::reset()
@@ -191,35 +202,51 @@ namespace ArrayUtilities
 	}
 
 	template<size_t root_node_count, typename TLinkType, size_t node_count, size_t node_width, typename TDataType>
+	inline wide_node_linked_list<root_node_count, TLinkType, node_count, node_width, TDataType>::iterator wide_node_linked_list<root_node_count, TLinkType, node_count, node_width, TDataType>::get_root_node_start(TLinkType root_node_index)
+	{
+		auto& itterator_target = *this;
+		return iterator(itterator_target,root_node_index);
+	}
+
+	template<size_t root_node_count, typename TLinkType, size_t node_count, size_t node_width, typename TDataType>
+	inline wide_node_linked_list<root_node_count, TLinkType, node_count, node_width, TDataType>::iterator wide_node_linked_list<root_node_count, TLinkType, node_count, node_width, TDataType>::end()
+	{
+		return wide_node_linked_list<root_node_count, TLinkType, node_count, node_width, TDataType>::iterator();
+	}
+
+	template<size_t root_node_count, typename TLinkType, size_t node_count, size_t node_width, typename TDataType>
 	inline void wide_node_linked_list<root_node_count, TLinkType, node_count, node_width, TDataType>::reset_root_nodes()
 	{
 		for (int i = 0; i < root_node_count; ++i)
 		{
-			root_node_ptrs[i].last_node = invalid_node_index;
-			root_node_ptrs[i].count = 0;
+			root_node_ptrs[i].write_node = invalid_node_index;
+			root_node_ptrs[i].write_index = node_width -1;
 		}
 	}
 
 	template<size_t root_node_count, typename TLinkType, size_t node_count, size_t node_width, typename TDataType>
 	inline void wide_node_linked_list<root_node_count, TLinkType, node_count, node_width, TDataType>::setup_free_node_list()
 	{
+		//nodes are organized in a loop so removing a node is the same even if its the first or last node
+		free_list_start = 0;
+
 		//do the first node
 		nodes[0].free_node_data.node_links.child_node = 1;
-		nodes[0].free_node_data.node_links.parent_node = invalid_node_index;
-		nodes[0].free_node_data.mark_as_free();
+		nodes[0].free_node_data.node_links.parent_node = node_count - 1;
+		nodes[0].mark_as_free();
 
 		//do all the middle nodex
 		for (int i = 1; i < (node_count -1); ++i)
 		{
 			nodes[i].free_node_data.node_links.child_node = i + 1;
 			nodes[i].free_node_data.node_links.parent_node = i - 1;
-			nodes[i].free_node_data.mark_as_free();
+			nodes[i].mark_as_free();
 		}
 		
 		//do the last node
-		nodes[node_count -1].free_node_data.node_links.child_node = invalid_node_index;
+		nodes[node_count -1].free_node_data.node_links.child_node = 0;
 		nodes[node_count -1].free_node_data.node_links.parent_node = node_count - 2;
-		nodes[node_count - 1].free_node_data.mark_as_free();
+		nodes[node_count -1].mark_as_free();
 	}
 
 
@@ -234,12 +261,12 @@ namespace ArrayUtilities
 	
 		typedef wide_node<node_width, TLinkType, TDataType> node_type;
 	
-		//using the root node count get the index in a wide node 
-		uint32 index_in_node = root_node_data.count % node_width;
+		//using the root node count get the index to write to
+		uint32 index_in_node = root_node_data.write_index + 1;
 
 		//check this item is the first element in a new wide node 
 		//which means an empty free node needs to be pulled from the free list
-		if (!index_in_node)
+		if (index_in_node == node_width)
 		{
 			//need to get node off the free list 
 			auto node_index = get_free_node();
@@ -247,63 +274,74 @@ namespace ArrayUtilities
 			assert(nodes[node_index].is_free(), "node pulled from free list is not initializesd to a free state");
 
 			//point this new node to the previous active head node
-			nodes[node_index].active_node_data.child_node = root_node_data.last_node;
+			nodes[node_index].active_node_data.child_node = root_node_data.write_node;
 
 			//update the node the root is pointing too
-			root_node_data.last_node = node_index;
+			root_node_data.write_node = node_index;
+
+			index_in_node = 0;
 		}
 
 		//copy across the data
-		nodes[root_node_data.last_node].active_node_data.data[index_in_node] = data;
+		nodes[root_node_data.write_node].active_node_data.data[index_in_node] = data;
 
-		//update the number of elements
-		root_node_data.count++;
+		//update the write index
+		root_node_data.write_index = index_in_node;
+
 	}
 	
 	template<size_t root_nodes, typename TLinkType, size_t node_count, size_t node_width, typename TDataType>
 	inline void wide_node_linked_list<root_nodes, TLinkType, node_count, node_width, TDataType>::remove(uint32 root_node_index, TDataType data)
 	{
 		//get the node the root points to 
-		root_node& root_node_data = root_node_ptrs[root_node_index];
+		root_node<TLinkType, node_count>& root_node_data = root_node_ptrs[root_node_index];
 
 		//get the number of nodes in the first half full node
-		uint32 first_node_entry_index = (root_node_data.count -1) % node_width;
+		uint32 first_node_entry_index = root_node_data.write_index;
 
-		uint32 write_node_index = root_node_data.last_node;
+		uint32 write_node_index = root_node_data.write_node;
 
-		uint32 active_node = root_node_data.last_node;
+		uint32 active_node = write_node_index;
 
 		uint32 active_node_read_index = first_node_entry_index;
 
-		for (int i = 0; i < node_count; ++i)
+		for (; active_node != invalid_node_index;)
 		{
-			//update the half fill node
-			for (;active_node_read_index >= 0; --active_node_read_index)
+			//does node match
+			if (nodes[active_node].active_node_data.data[active_node_read_index] == data)
 			{
-				//does node match
-				if (nodes[active_node].active_node_data.data[active_node_read_index] == data)
+				//copy across lead value
+				nodes[active_node].active_node_data.data[active_node_read_index] = nodes[write_node_index].active_node_data.data[first_node_entry_index];
+
+				//check if write node is empty 
+				if (!first_node_entry_index)
 				{
-					//coppy across lead value
-					nodes[active_node].active_node_data.data[active_node_read_index] = nodes[write_node_index].active_node_data.data[first_node_entry_index];
+					//clean up and update the point 
+					root_node_data.write_node = nodes[write_node_index].active_node_data.child_node;
 
-					//check if write node is empty 
-					if (!first_node_entry_index)
-					{
-						//clean up and update the point 
-						root_node_data.last_node = root_node_data.last_node.active_node_data.child_node;
-					}
+					//update the new read index
+					//this will be decremented into the actual read index later
+					root_node_data.write_index = node_width;
 
-					root_node_data.count--;
-
-					return;
+					//return last node to free list 
+					return_node(write_node_index);
 				}
+
+				root_node_data.write_index--;
+
+				return;
 			}
+		
+			bool is_last_item_in_node = static_cast<bool>(!active_node_read_index);
 
 			//reset the wide node read index
-			active_node_read_index = (node_width - 1);
+			active_node_read_index += (node_width * is_last_item_in_node);
 
 			//change read node to child of current node 
-			active_node = nodes[active_node].active_node_data.child_node;
+			active_node = (active_node * !is_last_item_in_node) + ( nodes[active_node].active_node_data.child_node * is_last_item_in_node);
+
+			//decrement index 
+			--active_node_read_index;
 		}
 
 		assert(false, "only here if no matching node found");
@@ -320,6 +358,10 @@ namespace ArrayUtilities
 	
 		//update the free list start
 		free_list_start = nodes[free_node].free_node_data.node_links.child_node;
+
+		//correctly link up the next node 
+		nodes[free_list_start].free_node_data.node_links.parent_node = nodes[free_node].free_node_data.node_links.parent_node;
+		nodes[nodes[free_list_start].free_node_data.node_links.parent_node].free_node_data.node_links.child_node = free_list_start;
 	
 		//return the index of the free node
 		return free_node;
@@ -328,14 +370,24 @@ namespace ArrayUtilities
 	template<size_t root_nodes, typename TLinkType, size_t node_count, size_t node_width, typename TDataType>
 	inline void wide_node_linked_list<root_nodes, TLinkType, node_count, node_width, TDataType>::return_node(TLinkType node_index)
 	{
-		//make sure there is nothing in this node 
-		assert(nodes[node_index].is_free() == true);
 	
+		auto existing_parent = nodes[free_list_start].free_node_data.node_links.parent_node;
+
 		//get the current free node head and point it to this new node
-		nodes[free_list_start].free_node_data.parent_node = node_index;
+		nodes[free_list_start].free_node_data.node_links.parent_node = node_index;
 	
-		nodes[node_index].free_node_data.child_node = free_list_start;
-	
+		nodes[node_index].free_node_data.node_links.child_node = free_list_start;
+
+		//link the new node to the other end of the free node loop  
+		//free nodes are linked in a loop so if a free node is detected at an index
+		//it can be removed from the free list without breaking the free list chain.
+		//this is node so an active node can just check if a free node exists at the next available index instead of getting a node from the start of the 
+		//queue which might not be in contigious memory 
+		nodes[node_index].free_node_data.node_links.parent_node = existing_parent;
+
+		nodes[existing_parent].free_node_data.node_links.child_node = node_index;
+		
+
 		free_list_start = node_index;
 	}
 
